5425545e5aa494fb73ace53afc286617aad2d2ea
ongw <wesjong@gmail.com>
2016-09-17 14:47:50 -0400

    Initial commit.

---
+++ mobile/src/main/java/com/wes/keyring/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wes.keyring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
